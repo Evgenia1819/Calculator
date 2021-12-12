9f39eebef27ec79e3c9391f90b0990c903cd30fe
Evgenia1819 <95176074+Evgenia1819@users.noreply.github.com>
2021-12-12 00:04:14 +0000

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,16 @@
+function calculator(firstNumber, operator, secondNumber) {
+  if (operator === '+') {
+     console.log((firstNumber + secondNumber).toFixed(2))
+}  else if (operator === '-') {
+     console.log((firstNumber - secondNumber).toFixed(2));
+}  else if (operator === '*'){
+     console.log((firstNumber*secondNumber).toFixed(2));
+}  else if (operator === '/') {
+     console.log((firstNumber / secondNumber).toFixed(2)); 
+}
+
+
+}
+
+calculator (5 ,'+', 10);
+calculator(25.5, '-', 3);
